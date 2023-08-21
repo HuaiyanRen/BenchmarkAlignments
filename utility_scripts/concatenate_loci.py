e99c2415aba6eeb27d4f210de406e9ff297bb4cd
HuaiyanRen <928003585@qq.com>
2023-08-21 17:04:41 +1000

    Update concatenate_loci.py

--- utility_scripts/concatenate_loci.py
+++ utility_scripts/concatenate_loci.py
@@ -3,28 +3,42 @@
 # cd to the infile before running
 
 from Bio.Nexus import Nexus
-#from Bio import AlignIO
 import os
+import argparse
 
-infile = r"C:\Users\u7151703\Desktop\research\datasets\Sanchez-Baracaldo_2017\nex"
+def concatenate_loci(inpath):
 
-file_list = [x for x in os.walk(infile)][0][2]
-print(file_list)
-full_file_list = [[x, os.path.join(infile, x)] for x in file_list]
+    #infile = r"C:\Users\u7151703\Desktop\research\datasets\Sanchez-Baracaldo_2017\nex"
 
-#try:
-#	file_list.remove(".DS_Store") # thanks Mac
-#except:
-#	pass
+    file_list = [x for x in os.walk(inpath)][0][2]
+    print(file_list)
 
-print("loading files")
-nexi =  [(fname[0], Nexus.Nexus(fname[1])) for fname in full_file_list]
+    full_file_list = [[x.split('_')[1], os.path.join(inpath, x)] for x in file_list] #full_file_list = [[x, os.path.join(inpath, x)] for x in file_list]
 
-print("combining alignments")
-combined = Nexus.combine(nexi)
+    print("loading files")
+    nexi =  [(fname[0], Nexus.Nexus(fname[1])) for fname in full_file_list]
 
-print("writing output")
-outfile = os.path.join(infile, "alignment.nex")
-outfile = open(outfile, 'w')
-combined.write_nexus_data(outfile)
-outfile.close()
+    print("combining alignments")
+    combined = Nexus.combine(nexi)
+
+    print("writing output")
+    outfile = os.path.join(inpath, "alignment.nex")
+    outfile = open(outfile, 'w')
+    combined.write_nexus_data(outfile)
+    outfile.close()
+    
+
+
+# running
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--inpath', '-i', help='', 
+                    default = r"C:\Users\u7151703\Desktop\research\datasets\processing\nex")
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    try:
+       concatenate_loci(args.inpath)
+    except Exception as e:
+        print(e)
+
+    
